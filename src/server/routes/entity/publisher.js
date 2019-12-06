74646e656d4c052ba548018ece8344d2ee47c9b9
Monkey Do <contact@monkeydo.digital>
2019-12-06 18:17:58 +0100

    feat(merge-tool): Merge Publishers

--- src/server/routes/entity/publisher.js
+++ src/server/routes/entity/publisher.js
@@ -238,6 +238,7 @@ function transformNewForm(data) {
 			data.publisherSection.endDate : '',
 		ended: data.publisherSection.ended,
 		identifiers,
+		mergingEntities: data.mergingEntities,
 		note: data.submissionSection.note,
 		relationships,
 		typeId: data.publisherSection.type
